b4d83d0cbf0ac25fa971de4abb325c7c36d61835
kose-uk45 <kosebilaliuk45@gmail.com>
2022-03-24 06:58:55 +0200

    chore(point of entry) setup apis for point of entry

--- api/v1/utils/auth.js
+++ api/v1/utils/auth.js
@@ -22,23 +22,6 @@ process.env.JWT_SCRETE_KEY,
 });
 
 
-
-// pagination for user data printed to return
-const pagination = (req, res, dataToPaginate) => {
-  const pageCount = Math.ceil(dataToPaginate.rows.length / 20);
-  let page = parseInt(req.query.page, 16);
-  if (!page) { page = 1; }
-  if (page > pageCount) {
-    page = pageCount;
-  }
-  res.status(200).json({
-    status: 200,
-    message: 'Operation successful',
-    pages: `${page} of ${pageCount}`,
-    data: dataToPaginate.rows.slice(page * 20 - 20, page * 20),
-  });
-};
-
 export {
-  hashPassword, generateTokens, veryfyPassword, pagination,
+  hashPassword, generateTokens, veryfyPassword,
 };
--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import apiRoutes from './api/v1/routes/routes';
-import { DHIS2_APIS, auth } from './api/v1/utils/authConfig';
+import vaccineCertRouter from './api/v1/routes/routes';
+import poeAirportRouter from './api/v1/routes/poeAirport';
 
-console.log('UTILS config', DHIS2_APIS, auth, process.env.PORT);
 const app = express();
 const port = process.env.PORT || 8000;
-// use builtin middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(cors());
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST'],
 }));
-// Custom middlewares for the app
-app.use('/api/v1/', apiRoutes.router);
+app.use('/api/v1/', vaccineCertRouter.router);
+app.use('/api/v1/poe/', poeAirportRouter.router);
 
-// generic error middleware function
 app.use((req, res) => {
   const error = new Error('Page not found!');
   error.status = 404;
